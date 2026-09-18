f15ad94949e3267f6fdf01b77b93f6189afc4944
Keleborn <22352763+Celandriel@users.noreply.github.com>
2026-09-18 11:36:24 -0700

    Strip Playerbot database core changes

--- src/server/database/Updater/DBUpdater.cpp
+++ src/server/database/Updater/DBUpdater.cpp
@@ -197,46 +197,6 @@ std::string DBUpdater<CharacterDatabaseConnection>::GetDBModuleName()
     return "characters";
 }
 
-#ifdef MOD_PLAYERBOTS
-// Playerbots Database
-template<>
-std::string DBUpdater<PlayerbotsDatabaseConnection>::GetConfigEntry()
-{
-    return "Updates.Playerbots";
-}
-
-template<>
-std::string DBUpdater<PlayerbotsDatabaseConnection>::GetTableName()
-{
-    return "Playerbots";
-}
-
-template<>
-std::string DBUpdater<PlayerbotsDatabaseConnection>::GetSourceDirectory()
-{
-    return BuiltInConfig::GetSourceDirectory() + "/modules/mod-playerbots";
-}
-
-template<>
-std::string DBUpdater<PlayerbotsDatabaseConnection>::GetBaseFilesDirectory()
-{
-    return DBUpdater<PlayerbotsDatabaseConnection>::GetSourceDirectory() + "/data/sql/playerbots/base/";
-}
-
-template<>
-bool DBUpdater<PlayerbotsDatabaseConnection>::IsEnabled(uint32 const updateMask)
-{
-    // This way silences warnings under msvc
-    return (updateMask & DatabaseLoader::DATABASE_PLAYERBOTS) ? true : false;
-}
-
-template<>
-std::string DBUpdater<PlayerbotsDatabaseConnection>::GetDBModuleName()
-{
-    return "db_playerbot";
-}
-#endif
-
 // All
 template<class T>
 BaseLocation DBUpdater<T>::GetBaseLocationType()
@@ -615,7 +575,3 @@ void DBUpdater<T>::ApplyFile(DatabaseWorkerPool<T>& pool, std::string const& hos
 template class AC_DATABASE_API DBUpdater<LoginDatabaseConnection>;
 template class AC_DATABASE_API DBUpdater<WorldDatabaseConnection>;
 template class AC_DATABASE_API DBUpdater<CharacterDatabaseConnection>;
-
-#ifdef MOD_PLAYERBOTS
-template class AC_DATABASE_API DBUpdater<PlayerbotsDatabaseConnection>;
-#endif
